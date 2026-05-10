9c5569d998ec0ede9e1f1fe01db1a21eccdfa8d1
Maria del Mar Diaz Ruiz <maria.diaz.ruiz@est.una.ac.cr>
2026-05-09 23:09:08 -0600

    Creacion de rama feature/admin, junto con creacion de un login basico (usuarios quemados por el momento, antes de hacer el jsonbin) y del panel administrativo de forma basica

--- proyecto-Cafe-UNA/src/router.jsx
+++ proyecto-Cafe-UNA/src/router.jsx
@@ -7,6 +7,8 @@ import {
 
 import Home from "./Pages/Home";
 import AboutUs from "./Pages/AboutUs";
+import Login from "./Pages/Login";
+import AdminPanel from "./Pages/AdminPanel";
 import Footer from "./Components/Footer";
 import Navbar from './Components/Navbar';
 
@@ -33,9 +35,21 @@ const AboutUsRoute = createRoute({
     path: "/AboutUs",
     component: AboutUs,
 })
+const loginRoute = createRoute({
+    getParentRoute: () => rootRoute,
+    path: "/login",
+    component: Login,
+})
+const adminPanelRoute = createRoute({
+    getParentRoute: () => rootRoute,
+    path: "/admin",
+    component: AdminPanel,
+})
 const routeTree= rootRoute.addChildren([
     home,
-    AboutUsRoute
+    AboutUsRoute,
+    loginRoute,
+    adminPanelRoute
 ])
 export const router = createRouter({
     routeTree
